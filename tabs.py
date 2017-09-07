3fbfca519faae159886ba45bd10dfaa4e19df102
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-07 10:10:31 +0530

    Update tabs.py

--- tabs.py
+++ tabs.py
@@ -49,3 +49,48 @@ if guess != 5:
         print("Sorry, better luck next time")
 else:
     print("Kuddos, you did it in the first go")
+
+# age = int(input("How old are you ? "))
+
+# Incase of AND stmt, python checks both the conditions and if they're true then only the loop will execute
+# if (age >= 18) and (age <= 60):
+# if 18 <= age <= 60:
+#     print("Have a good day working !!")
+
+# Whereas incase of OR stmt, python checks only any one of the two conditions and if any one of them evaluates to true
+# Then if will run the IF loop otherwise it will jump to the ELSE loop
+# if (age < 18) or (age > 60):
+#     print("Enjoy your life")
+# else:
+#     print("Have a good day working!!!")
+
+# Example of FALSE stmts
+# x = input("Please enter something : ")
+#
+# # If you enter anything this loop will be executed
+# if x:
+#     print("You entered '{}'".format(x))
+# else:
+#     print("You didn't enter anything!!")
+
+# Example of NOT
+# print(not True)
+# print(not False)
+#
+# age = int(input("How old are you ? "))
+#
+# if not(age < 18):
+#     print("Hurray, You can vote !!")
+#     print("Please put X in the box")
+# else:
+#     print("Please come back in {} years !".format(18 - age))
+
+# Example of IN keyword
+parrot = "Cute Bird"
+
+letter = input("Please enter a character : ")
+
+if letter not in parrot:
+    print("Sorry, i don't need that letter")
+else:
+    print("Give me {}, Anuj".format(letter))
