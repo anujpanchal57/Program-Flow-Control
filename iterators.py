22de52e11963b4f3d686aac5d5ee1c5d514669bb
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-15 16:50:21 +0530

    Add files via upload

---
+++ iterators.py
@@ -0,0 +1,30 @@
+string = "0123456789"
+
+# for char in string:
+#     print(char)
+
+# my_iterator = iter(string)
+#
+# # Returns the object value of the iterator
+# print(my_iterator)
+# # Returns the next values in the Iterator
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+#
+# # And, if we add an extra print function for the next iterator, it will give us an error
+# print(next(my_iterator))
+
+for char in string:
+    print(char)
+
+# Output will be the same as the output of FOR loop
+for char in iter(string):
+    print(char)
