78e4cc7dea0964770914652d4fd626621c9ba63c
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-16 08:36:09 +0530

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -1,26 +1,26 @@
 string = "0123456789"
 
-# for char in string:
-#     print(char)
+for char in string:
+    print(char)
 
-# my_iterator = iter(string)
-#
-# # Returns the object value of the iterator
-# print(my_iterator)
-# # Returns the next values in the Iterator
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-# print(next(my_iterator))
-#
-# # And, if we add an extra print function for the next iterator, it will give us an error
-# print(next(my_iterator))
+my_iterator = iter(string)
+
+# Returns the object value of the iterator
+print(my_iterator)
+# Returns the next values in the Iterator
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+print(next(my_iterator))
+
+# And, if we add an extra print function for the next iterator, it will give us an error
+print(next(my_iterator))
 
 for char in string:
     print(char)
