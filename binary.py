5e3be5cdcbb82b84c4acd90bfab917c527ccfab8
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-18 18:31:07 +0530

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,16 @@
+# # Prints the numbers 1 to 16 in their binary format
+# for i in range(17):
+#     print("{0:>2} in binary is: {0:>08b}".format(i))
+#
+# # Printing Hexadecimal numbers
+# for i in range(17):
+#     print("{0:>2} in hex is {0:>02x}".format(i))
+
+x = 0x20
+y = 0x0a
+print(x)
+print(y)
+print(x * y)
+
+# Prints the binary equivalent, which is 42
+print(0b00101010)
