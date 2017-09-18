94b159e0c4d1ad607ac4547e7b45c7db16d2aed0
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-18 18:33:12 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,10 +1,10 @@
-# # Prints the numbers 1 to 16 in their binary format
-# for i in range(17):
-#     print("{0:>2} in binary is: {0:>08b}".format(i))
-#
-# # Printing Hexadecimal numbers
-# for i in range(17):
-#     print("{0:>2} in hex is {0:>02x}".format(i))
+# Prints the numbers 1 to 16 in their binary format
+for i in range(17):
+    print("{0:>2} in binary is: {0:>08b}".format(i))
+
+# Printing Hexadecimal numbers
+for i in range(17):
+    print("{0:>2} in hex is {0:>02x}".format(i))
 
 x = 0x20
 y = 0x0a
